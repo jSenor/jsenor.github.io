9834cbe7932aa511d4158fcead0a26e9cec484e4
Wisdom <jslord21@gmail.com>
2017-04-17 11:06:43 +0100

    Make the navigation responsive

--- build/javascripts/script.js
+++ build/javascripts/script.js
@@ -1,6 +1,50 @@
 var nameIntro = document.querySelector(".name-intro");
 console.log(nameIntro);
 Typed.new(".intro", {
-	strings: ["<div class=\"name-intro\">Hi there, am <b>Wisdom Ogwu</b></div><div class=\"what-i-do\">I am a <b>Front End Designer</b>, <b>Back End Developer</b> and this is my portfolio</div>"],
+	strings: ["<div class=\"name-intro\">Hi there, am <b>Wisdom Ogwu</b></div><div class=\"what-i-do\">I am a <b>Front End Designer</b>, <b>Back End Developer</b> and this is <b>My Portfolio</b></div>"],
 	typeSpeed: 5
 });
+
+addEvents();
+
+function addEvents(){
+
+	addOpenNavEvent();
+	addCloseNavEvent();
+
+}
+
+function addOpenNavEvent(){
+	try{
+		var toggle = document.querySelector(".open-nav");
+		var nav = document.querySelector(".navigation ul");
+		var close = document.querySelector(".close-nav");
+		toggle.onclick = function(event){
+			nav.style.display = "block";
+			toggle.style.display = "none";
+			close.style.display = "flex";
+			console.log(event);
+		}
+	}
+
+	catch(error){
+		console.log(error);
+	}
+}
+
+function addCloseNavEvent(){
+	try{
+		var close = document.querySelector(".close-nav");
+		var nav = document.querySelector(".navigation ul");
+		var toggle = document.querySelector(".open-nav");
+		close.onclick = function(event){
+			nav.style.display = "none";
+			toggle.style.display = "flex";
+			close.style.display = "none";
+		}
+	}
+
+	catch(error){
+		console.log(error);
+	}
+}
